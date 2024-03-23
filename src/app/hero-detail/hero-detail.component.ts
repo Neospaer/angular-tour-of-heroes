ffa7061d6b6518ce466c0a3beeabe767c525f239
maksim_repnikov <maks.repnikov32@mail.ru>
2024-03-23 14:09:48 +0300

    Add

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Hero }         from '../hero';
+import { HeroService }  from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: [ './hero-detail.component.css' ]
+})
+export class HeroDetailComponent implements OnInit {
+  hero: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const param=this.route.snapshot.paramMap.get('id');
+    const id = param?+param:0;
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+   }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
